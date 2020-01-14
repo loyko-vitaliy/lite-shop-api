b7a2c2223974996c1b78b01bf534b43c6305ccac
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-01-14 20:06:33 +0200

    feat(routes) add routes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const {routes} = require('./routes');
 
 const {PORT = 3000, NODE_ENV} = process.env;
 
 const app = express();
-app.use(express.json());
 
-app.use((req, res) => res.send('Init application'));
-
-app.listen(
-    PORT,
-    console.log(`Server has been started in ${NODE_ENV} mode at port ${PORT}`)
-);
+// prettier-ignore
+app
+    .use(express.json())
+    .use('/api', routes)
+    .listen(
+        PORT,
+        console.log(
+            `Server has been started in ${NODE_ENV} mode at port ${PORT}`
+        )
+    );
