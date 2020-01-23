c2c94f00bee8a22a190725263839e5386e294141
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-01-23 23:20:41 +0200

    feat(index.js) connect notFoundHandler and errorHandler middleware

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const {routes} = require('./routes');
 const {bootstrap} = require('./bootstrap');
+const notFoundHandler = require('./middleware/not-found-handler');
+const errorHandler = require('./middleware/error-handler.js');
 
 const {PORT = 3000, NODE_ENV} = process.env;
 
@@ -11,6 +13,8 @@ bootstrap();
 app
     .use(express.json())
     .use('/api', routes)
+    .use(notFoundHandler)
+    .use(errorHandler)
     .listen(
         PORT,
         console.log(
