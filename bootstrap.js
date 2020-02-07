e1c74211cf8f6f24911f86a45d713a8d0039a79e
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-02-07 22:07:35 +0200

    fix(bootstrap) fix loading priority

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 const serviceLocator = require('./services/service.locator');
 
-exports.bootstrap = () => {
-    serviceLocator.register('connection', require('knex')(require('./knexfile')));
-};
+serviceLocator.register('connection', require('knex')(require('./knexfile')));
+
+module.exports = () => {};
