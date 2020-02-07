004b26217f058824e342d46e3d8a243c377ac384
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-02-07 22:43:12 +0200

    feat(routes) add auth routes

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,20 @@ const UserController = require('../controllers/user.controller');
 const CategoryController = require('../controllers/category.controller');
 const OrderController = require('../controllers/order.controller');
 const ProductController = require('../controllers/product.controller');
+const AuthController = require('../controllers/auth.controller');
+const {Roles, disallowRoles, allowRoles} = require('../middleware/check-roles');
 
-attachResourceController('/users', UserController);
+attachResourceController('/auth', AuthController, {
+    middleware: [],
+    bindings: [
+        {route: '/login', method: 'POST', action: 'login', middleware: []},
+        {route: '/logout', method: 'POST', action: 'logout', middleware: [disallowRoles(Roles.GUEST)]},
+        {route: '/register', method: 'POST', action: 'register', middleware: []},
+    ],
+});
+attachResourceController('/users', UserController, {middleware: [allowRoles(Roles.ADMIN)]});
 attachResourceController('/categories', CategoryController);
-attachResourceController('/orders', OrderController);
+attachResourceController('/orders', OrderController, {middleware: [disallowRoles(Roles.GUEST)]});
 attachResourceController('/products', ProductController);
 
 exports.routes = router;
