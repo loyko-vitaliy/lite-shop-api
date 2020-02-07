e1045e0691a1eb0aed8bff8921ee1fc3f2a2f612
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-02-07 22:32:37 +0200

    feat(passport) add passport config

---
+++ passport.js
@@ -0,0 +1,58 @@
+const passport = require('passport');
+const {Strategy: LocalStrategy} = require('passport-local');
+const {Strategy: BearerStrategy} = require('passport-http-bearer');
+const uuid = require('uuid/v4');
+const User = require('./models/user.model');
+
+passport.use(
+    'login',
+    new LocalStrategy({usernameField: 'username', passwordField: 'password'}, async (username, password, done) => {
+        try {
+            const user = await User.query().findOne({username});
+            if (!user) {
+                return done(null, false, {message: 'Wrong credentials supplied.'});
+            }
+            const passwordsAreEqual = await user.verifyPassword(password);
+            if (!passwordsAreEqual) {
+                return done(null, false, {message: 'Wrong credentials supplied.'});
+            }
+            const token = uuid();
+            await User.query().patchAndFetchById(user.id, {token});
+            return done(null, {id: user.id, token}, {message: 'Logged in successfully!'});
+        } catch (error) {
+            return done(error);
+        }
+    })
+);
+
+passport.use(
+    new BearerStrategy({passReqToCallback: true}, async (req, token, done) => {
+        try {
+            const user = await User.query().findOne({token});
+            if (!user) {
+                return done(null, false, {message: `There is no user with token: ${token}`});
+            }
+            return done(null, user);
+        } catch (error) {
+            done(error);
+        }
+    })
+);
+
+passport.use(
+    'register',
+    new LocalStrategy(
+        {usernameField: 'username', passwordField: 'password', passReqToCallback: true},
+        async ({body}, username, password, done) => {
+            try {
+                const token = uuid();
+                const user = await User.query().insertAndFetch({...body, token});
+                return done(null, user, {message: 'User created successfully!'});
+            } catch (error) {
+                return done(error);
+            }
+        }
+    )
+);
+
+module.exports = passport;
