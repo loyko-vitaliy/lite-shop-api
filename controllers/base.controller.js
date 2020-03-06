7e75d33f7ee4866f38cb99c849c91de47cf00db3
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-03-07 01:56:31 +0200

    feat(controllers) add filter to base controller

--- controllers/base.controller.js
+++ controllers/base.controller.js
@@ -1,5 +1,4 @@
 const successResponse = require('../helpers/success-response');
-const {buildFilter} = require('objection-filter');
 class BaseController {
     constructor() {
         this.model = require(`../models/${this.modelName}.model.js`);
@@ -10,11 +9,58 @@ class BaseController {
         return name.toLocaleLowerCase();
     }
 
-    async index(req, res) {
-        if (!req.query.filter) {
-            return res.status(200).json(successResponse(await this.model.query()));
-        }
-        res.status(200).json(successResponse(await buildFilter(this.model).build(JSON.parse(req.query.filter))));
+    async index({query: {filter = '{}'}}, res) {
+        const {
+            limit = 10,
+            offset = 0,
+            order = [],
+            json: jsonFilters = {},
+            relations: relationFilters = {},
+            ...filters
+        } = await JSON.parse(filter);
+
+        const orderRules = order.map(rule => {
+            const [column, order] = rule.split(' ');
+            return {column, order};
+        });
+
+        const jsonRules = jsonFilters => {
+            const rules = {};
+            for (const filter in jsonFilters) {
+                for (const field in jsonFilters[filter]) {
+                    rules[field] = jsonFilters[filter][field];
+                }
+            }
+
+            return rules;
+        };
+
+        const relationFiltersRules = filters => {
+            const filterRules = {};
+            for (const filter in filters) {
+                for (const field in filters[filter]) {
+                    filterRules[`${filter}.${field}`] = filters[filter][field];
+                }
+            }
+            return filterRules;
+        };
+
+        res.status(200).json(
+            successResponse(
+                await this.model
+                    .query()
+                    .modify(builder =>
+                        Object.keys(jsonFilters).length > 0
+                            ? builder.whereJsonSupersetOf(Object.keys(jsonFilters)[0], jsonRules(jsonFilters))
+                            : null
+                    )
+                    .joinRelated(`${Object.keys(relationFilters)}`)
+                    .where(relationFiltersRules(relationFilters))
+                    .orderBy(orderRules)
+                    .where(filters)
+                    .range(offset, limit + offset - 1)
+            )
+        );
     }
 
     async create(req, res) {
