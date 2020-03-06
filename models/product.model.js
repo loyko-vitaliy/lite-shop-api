dddb6e51c13f3e18052ba8fb4db90573b9162255
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-03-07 01:55:11 +0200

    feat(models) add category relationMapping to products

--- models/product.model.js
+++ models/product.model.js
@@ -1,8 +1,23 @@
+const {Model} = require('objection');
 const Base = require('./base.model');
+const Category = require('../models/category.model');
 
 class Product extends Base {
     static get tableName() {
         return 'products';
+    }
+
+    static relationMappings() {
+        return {
+            categories: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Category,
+                join: {
+                    from: 'products.categoryId',
+                    to: 'categories.id',
+                },
+            },
+        };
     }
 }
 
