7057857abf79a096ef29ada8a2b1f951f2bc509e
Vitaliy Loyko <vitaliy.87@icloud.com>
2020-03-26 17:16:24 +0200

    refactor(seeds) refactor categories seed

--- api/seeds/categories.js
+++ api/seeds/categories.js
@@ -7,11 +7,11 @@ exports.seed = knex =>
                 {name: 'MacBook', slug: 'macbook', description: 'Лучшие ноутбуки', parent_id: 1},
                 {name: 'MacBook Air', slug: 'macbook-air', description: 'Лучшие ноутбуки MacBook Air', parent_id: 2},
                 {name: 'MacBook Pro', slug: 'macbook-pro', description: 'Лучшие ноутбуки MacBook Pro', parent_id: 2},
-                {name: 'iPhone', slug: 'iPhone', description: 'Лучшие смартфоны iPhone', parent_id: 1},
+                {name: 'iPhone', slug: 'iphone', description: 'Лучшие смартфоны iPhone', parent_id: 1},
                 {name: 'iPhone 11', slug: 'iphone-11', description: 'Лучшие смартфоны iPhone 11', parent_id: 5},
                 {name: 'iPhone 10', slug: 'iphone-10', description: 'Лучшие смартфоны iPhone 10', parent_id: 5},
-                {name: 'iPad', slug: 'ipad', description: 'Лучшие смартфоны iPad', parent_id: 1},
-                {name: 'iPod', slug: 'ipod', description: 'Лучшие смартфоны iPod', parent_id: 1},
-                {name: 'Apple Watch', slug: 'apple-watch', description: 'Лучшие смартфоны Apple Watch', parent_id: 1},
+                {name: 'iPad', slug: 'ipad', description: 'Лучшие планшеты iPad', parent_id: 1},
+                {name: 'iPod', slug: 'ipod', description: 'Лучшие музыкальные плееры iPod', parent_id: 1},
+                {name: 'Apple Watch', slug: 'apple-watch', description: 'Лучшие умные часы Apple Watch', parent_id: 1},
             ])
         );
